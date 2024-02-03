f805fb110a37ff054e6971416f7761a89d06bcc5
JN Hernández <jes.nat.hernandez@gmail.com>
2024-02-03 11:38:49 -0700

    Update Special Character check
    A couple test cases revealed that the backslash,
    quotation mark, and apostrophe mark were not being
    caught by the special character check.  Separated
    this out into its own function and reviewed the
    regular expression (re) docs to update the check.

--- codelandUsernameValidation/codelandUsernameValidation.py
+++ codelandUsernameValidation/codelandUsernameValidation.py
@@ -3,16 +3,21 @@
 # Python 3.8.1
 # Problem: https://coderbyte.com/editor/Codeland%20Username%20Validation:Python3
 import re
+import string
+
+
+def checkSpecialCharacters(userInput):
+    regex = re.compile('[@!#$%^&*()<\\\\">?/|}\'{~:]')
+    if regex.search(userInput) is None:
+        return True
+    else:
+        return False
 
 
 def checkEndsWithUnderscore(userInput):
-    regex = re.compile('[@!#$%^&*()<>?/\|}{~:]')
-    if regex.search(userInput) is None:
-        last_letter = userInput[-1]
-        if last_letter != "_":
-            return True
-        else:
-            return False
+    last_letter = userInput[-1]
+    if last_letter != "_":
+        return True
     else:
         return False
 
@@ -36,7 +41,10 @@ def checkUsernameLength(userInput):
 def codelandUsernameValidation(strParam):
     if checkUsernameLength(strParam):
         if checkStartsWithLetter(strParam):
-            return checkEndsWithUnderscore(strParam)
+            if checkSpecialCharacters(strParam):
+                return checkEndsWithUnderscore(strParam)
+            else:
+                return False
         else:
             return False
     else:
