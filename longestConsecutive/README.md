0e59413bd31fa464f76cd292257cdb9d94790c62
JN Hernández <jes.nat.hernandez@gmail.com>
2024-02-09 19:19:58 -0700

    Added longestConsecutive python exercise
    Added another python exercise from the CoderByte site.

---
+++ longestConsecutive/README.md
@@ -0,0 +1,23 @@
+# Longest Consecutive
+Problem Source: [CoderByte Link](https://coderbyte.com/information/Longest%20Consecutive)
+
+# Success Criteria
+When given a list of numbers, for example, `4 3 8 1 2 6 100 9`, break the list into consecutive number lists.
+
+Example: `[1, 2, 3, 4]`, `[6]`, `[8, 9]`, `[100]`
+
+Then, count how many numbers are in the longest consecutive string.  In this example, `4` will be returned.
+
+# Success Examples
+```
+Input: 77 80 79 21 20 2 3 1 81 
+Output: 3
+```
+```
+Input: 6 7 14 5 15 9 64 78 8 32
+Output: 5
+```
+
+# Script Details
+
+- Python 3.8.10
---
+++ longestConsecutive/longest_consecutive.py
@@ -0,0 +1,47 @@
+"""
+Codeland Longest Consecutive exercise
+"""
+
+
+def convert_user_input_to_array(user_input):
+    """Take user input and convert to array"""
+    input_list = user_input.split()
+    try:
+        integer_array = [int(num) for num in input_list]
+        integer_array = [num for num in integer_array if num > 0]
+        return integer_array
+
+    except ValueError:
+        integer_array = []
+        return integer_array
+
+
+def check_array_valid(array):
+    """Check to see if the array is valid"""
+    if not array:
+        array_validity = "Invalid input! Please enter positive integers separated by spaces."
+    else:
+        array_validity = find_longest_consecutive(array)
+    return array_validity
+
+
+def find_longest_consecutive(array):
+    """Find the longest consecutive list of numbers"""
+    array.sort()
+    current_length = 1
+    longest_length = 1
+    for i in range(1, len(array)):
+        if array[i] == array[i - 1] + 1:
+            current_length += 1
+        elif array[i] != array[i - 1]:
+            current_length = 1
+        longest_length = max(longest_length, current_length)
+
+    return longest_length
+
+if __name__ == '__main__':
+    # example_input: 4 3 8 1 2 6 100 9
+    original_user_input = input()
+    array_to_test = convert_user_input_to_array(original_user_input)
+    result = check_array_valid(array_to_test)
+    print(result)
